be39b1304c644baef27a7d212a1a57a6d791ba96
Lee SunBowen <1554849150@qq.com>
2020-10-24 13:43:14 +0800

    修改内容

--- 发邮件.py
+++ 发邮件.py
@@ -4,22 +4,32 @@ Created on Sat Oct 24 13:22:01 2020
 
 @author: LiSunBowen
 """
-def sendmail(txt):
+def sendmail(txt,sd,rc,ht,un,pw，fn,rn):
+    '''
+    txt:邮件文本
+    sd：发送者邮箱地址
+    rc：接收者邮箱地址
+    ht: host 服务器地址
+    un: 用户名
+    pw: 密码（口令）
+    fn: 发件人称呼
+    rn: 收件人称呼
+    '''
     import smtplib
     from email.mime.text import MIMEText
     from email.header import Header
 
     # 第三方 SMTP 服务
-    mail_host="smtp.qq.com"  #设置服务器
-    mail_user="1554849150@qq.com"    #用户名
-    mail_pass="iugqrxsaqrfxiiha"   #口令
+    mail_host= ht  #设置服务器
+    mail_user= un    #用户名
+    mail_pass = pw # 口令
 
-    sender = '1554849150@qq.com'
-    receivers = ['1554849150@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+    sender = sd
+    receivers = [rc]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
     message = MIMEText(txt, 'plain', 'utf-8')
-    message['From'] = Header("博闻的机器人", 'utf-8')
-    message['To'] =  Header("你", 'utf-8')
+    message['From'] = Header(fn, 'utf-8')
+    message['To'] =  Header(rn, 'utf-8')
 
     subject = 'Python SMTP 邮件测试'
     message['Subject'] = Header(subject, 'utf-8')
